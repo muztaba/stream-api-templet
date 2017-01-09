c371b41bd97c527ab66d82c2d5889320743bd0ee
seal <hasanat.muztaba@gmail.com>
2017-01-10 00:18:25 +0600

    find name of dept where avg salary max

--- src/Main.java
+++ src/Main.java
@@ -26,10 +26,19 @@ public class Main {
         OptionalDouble avg = Arrays.stream(a)
                 .average();
 
-           // other way
+        // other way
         double doubleAvg = IntStream.of(a)
                 .summaryStatistics()
                 .getAverage();
+
+        // Find the department from list which average salary of employees maximum
+        String  deptName = Util.nodeList().stream()
+                .collect(Collectors.groupingBy(Node::getDept, Collectors.averagingDouble(Node::getSalary)))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .get()
+                .getKey();
     }
 }
 
@@ -38,5 +47,43 @@ class Util {
         return Stream.of("1 SEAL", "5 HASANAT", "3 MUZTABA", "2 PJ")
                 .map(i -> i.split(" "))
                 .collect(Collectors.toMap((i -> i[1]), i -> Integer.parseInt(i[0])));
+    }
+
+    static List<Node> nodeList() {
+        return Arrays.asList(new Node("A", "CSE", 5500),
+                new Node("B", "CSE", 5600),
+                new Node("C", "CSE", 4500),
+                new Node("D", "EEE", 7000),
+                new Node("E", "EEE", 3400),
+                new Node("F", "EEE", 8000),
+                new Node("G", "ECO", 4000),
+                new Node("H", "ECO", 5000),
+                new Node("I", "ECO", 7000));
+
+    }
+}
+
+
+class Node {
+    String name;
+    String dept;
+    int salary;
+
+    public Node(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public int getSalary() {
+        return salary;
     }
 }
